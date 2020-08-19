c3c2e96da34b6cd7709f5993dbfb22a5d84a56e4
lixzzzzzz <lixzzzzzz@126.com>
2020-08-19 10:59:30 +0800

    L647_回文子串

---
+++ Swift/回文.playground/Pages/L647_回文子串.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+//: [Previous](@previous)
+
+import Foundation
+
+class Solution {
+    private var count = 0;
+    func countSubstrings(_ s: String) -> Int {
+        if s.count == 0 {
+            return 0
+        }
+        var chars = Array(s)
+        for i in 0..<s.count {
+            helper(chars,i,i)
+            helper(chars,i,i+1)
+        }
+        return count;
+    }
+
+    private func helper(_ chars:[String.Element],_ left:Int,_ right:Int) {
+        var l = left
+        var r = right
+        while l >= 0 && r < chars.count && chars[l] == chars[r]{
+            count += 1
+            l -= 1
+            r += 1
+        }
+    }
+}
+
+//: [Next](@next)
