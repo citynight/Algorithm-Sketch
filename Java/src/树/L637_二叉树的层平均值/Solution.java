8cf2ac302134822e62ff7536d1078fabe12de669
lixzzzzzz <lixzzzzzz@126.com>
2020-09-12 18:45:56 +0800

    L637_二叉树的层平均值

---
+++ Java/src/树/L637_二叉树的层平均值/Solution.java
@@ -0,0 +1,40 @@
+package 树.L637_二叉树的层平均值;
+
+import Common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author logan
+ * @date 2020/9/12 6:17 下午
+ */
+public class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                sum += cur.val;
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right)
+                }
+            }
+            res.add(sum/size);
+        }
+        return res;
+    }
+}
